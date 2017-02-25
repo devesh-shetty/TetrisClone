665c9429d531367aa14a657d93b2437d17866d81
Devesh Shetty <devesh.shetty007@gmail.com>
2017-02-25 18:56:06 +0530

    Restricted Tetromino movement to within the grid

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Game : MonoBehaviour {
 
+	public static int gridWidth = 10;
+	public static int gridHeight = 20;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +16,13 @@ public class Game : MonoBehaviour {
 	void Update () {
 		
 	}
+
+	public bool CheckIsInsideGrid(Vector2 pos){
+		return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
+	}
+
+	public Vector2 Round(Vector2 pos){
+		return new Vector2 (Mathf.Round(pos.x), Mathf.Round(pos.y));
+	}
+
 }
--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -23,21 +23,60 @@ public class Tetromino : MonoBehaviour {
 
 			transform.position += new Vector3 (1, 0, 0);
 
+			if (CheckIsValidPosition ()) {
+			
+			} else {
+				transform.position += new Vector3 (-1, 0, 0);
+			}
+
 		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
 
 			transform.position += new Vector3 (-1, 0, 0);
 
+			if (CheckIsValidPosition ()) {
+
+			} else {
+				transform.position += new Vector3 (1, 0, 0);
+			}
+
 		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
 
 			transform.Rotate (0, 0, 90);
-			
+
+			if (CheckIsValidPosition ()) {
+
+			} else {
+				transform.Rotate (0, 0, -90);
+			}
+
 		} else if(Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed){
 
 			transform.position += new Vector3 (0, -1, 0);
+
+			if (CheckIsValidPosition ()) {
+
+			} else {
+				transform.position += new Vector3 (0, 1, 0);
+			}
+
 			fall = Time.time;
 		}
 
 
 	}
+
+	bool CheckIsValidPosition(){
+		foreach (Transform mino in transform) {
+			Vector2 pos = FindObjectOfType<Game> ().Round (mino.position);
+
+			if (FindObjectOfType<Game> ().CheckIsInsideGrid (pos) == false) {
+				return false;
+			}
+
+		}
+
+		return true;
+	}
+
 
 }
