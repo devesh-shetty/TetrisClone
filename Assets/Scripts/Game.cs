3adcdd5f22b36828ec5ff52ae23191e38b35a4a6
Devesh Shetty <devesh.shetty007@gmail.com>
2017-02-25 19:32:41 +0530

    Spawning random tetromino

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -9,12 +9,17 @@ public class Game : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		SpawnNextTetromino ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void SpawnNextTetromino(){
+		GameObject nextTetromino = (GameObject)Instantiate (Resources.Load(GetRandomTetromino(), typeof(GameObject)), new Vector2(5.0f, 20.0f),
+			Quaternion.identity);
 	}
 
 	public bool CheckIsInsideGrid(Vector2 pos){
@@ -23,6 +28,45 @@ public class Game : MonoBehaviour {
 
 	public Vector2 Round(Vector2 pos){
 		return new Vector2 (Mathf.Round(pos.x), Mathf.Round(pos.y));
+	}
+
+	string GetRandomTetromino(){
+		int randomTetromino = Random.Range (1, 8);
+
+		string randomTetrominoName = "Prefabs/Tetromino_T";
+
+		switch (randomTetromino) {
+		case 1:
+			randomTetrominoName = "Prefabs/Tetromino_T";
+			break;
+
+		case 2:
+			randomTetrominoName = "Prefabs/Tetromino_Long";
+			break;
+
+		case 3: 
+			randomTetrominoName = "Prefabs/Tetromino_Square";
+			break;
+		
+		case 4:
+			randomTetrominoName = "Prefabs/Tetromino_J";
+			break;
+
+		case 5:
+			randomTetrominoName = "Prefabs/Tetromino_L";
+			break;
+		
+		case 6:
+			randomTetrominoName = "Prefabs/Tetromino_S";
+			break;
+		
+		case 7:
+			randomTetrominoName = "Prefabs/Tetromino_Z";
+			break;
+		}
+
+		return randomTetrominoName;
+
 	}
 
 }
