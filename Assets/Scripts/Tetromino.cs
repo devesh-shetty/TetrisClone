a0c397090626767339489d3f1969f3996acfc2c1
Devesh Shetty <devesh.shetty007@gmail.com>
2017-02-25 19:08:18 +0530

    Restricted rotation for tetromino

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -7,6 +7,9 @@ public class Tetromino : MonoBehaviour {
 	float fall = 0;
 	public float fallSpeed = 1;
 
+	public bool allowRotation = true;
+	public bool limitRotation = false;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -41,12 +44,34 @@ public class Tetromino : MonoBehaviour {
 
 		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
 
-			transform.Rotate (0, 0, 90);
+			if (allowRotation) {
 
-			if (CheckIsValidPosition ()) {
+				if (limitRotation) {
+					if (transform.rotation.eulerAngles.z >= 90) {
+						transform.Rotate (0, 0, -90);
+					} else {
+						transform.Rotate (0,0,90);
+					}
 
-			} else {
-				transform.Rotate (0, 0, -90);
+				} else {
+					transform.Rotate (0, 0, 90);
+				}
+				if (CheckIsValidPosition ()) {
+
+				} else {
+
+					if (limitRotation) {
+						if (transform.rotation.eulerAngles.z >= 90) {
+							transform.Rotate (0, 0, -90);
+						} else {
+							transform.Rotate (0, 0, 90);
+						}
+					} else {
+						transform.Rotate (0, 0, -90);
+					}
+
+				}
+
 			}
 
 		} else if(Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed){
